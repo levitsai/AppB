0f4fcc0cb24e1bb84f7a5bbfa64307b4d41db793
LeviTsai <pisces.lord@gmail.com>
2018-09-11 23:19:56 +0800

    Initial Commit

---
+++ AppB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppB
+//
+//  Created by YI-LUN TSAI on 2018/9/11.
+//  Copyright © 2018年 YI-LUN TSAI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
